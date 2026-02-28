d918eaa2f94322e1fd61f53d80d31872115c398f
Dudu2231 <eduardonascimento2231@gmail.com>
2026-02-28 18:46:30 -0300

    adicionar configs de deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://EduardoSantos2231.github.io',
+  base: '/visuDSA',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
